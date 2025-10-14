6ccdee2fd0f50ead9793c3e97b419023b361c1ab
yukin <hmgm.spec@gmail.com>
2025-10-14 19:47:49 +0900

    SQLで実装した

---
+++ flaskr/schema.sql
@@ -0,0 +1,16 @@
+-- blogsテーブルが既にあったら一度消すSQL
+DROP TABLE IF EXISTS blogs;
+
+-- blogsテーブルを作るSQL
+CREATE TABLE blogs (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
+INSERT INTO blogs (id,title, body, user_name)
+VALUES (1,'ごはん日記', 'お昼はカルボナーラ', 'yuki'),
+(2,'好きな曲リスト', 'インフェルノ/ Mrs.Gただ君に晴れ/ヨルシカ', 'mai'),
+(3, '気になる本', 'ファッション・パーツ図鑑', 'yuki');
